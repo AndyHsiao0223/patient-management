44ee37b22b72436bbffc6733e8b033545ba27d23
Andy Hsiao <hs074@heshuosg.com>
2026-01-29 17:54:34 +0800

    feat: Implement createPatient endpoint

--- patient-service/src/main/java/com/example/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/example/patientservice/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.example.patientservice.mapper;
 
+import com.example.patientservice.dto.PatientRequestDTO;
 import com.example.patientservice.dto.PatientResponseDTO;
 import com.example.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
@@ -13,5 +16,16 @@ public class PatientMapper {
         patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
 
         return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO PatientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(PatientRequestDTO.getName());
+        patient.setEmail(PatientRequestDTO.getEmail());
+        patient.setAddress(PatientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(PatientRequestDTO.getDateOfBirth()));
+        patient.setRegistrationDate(LocalDate.now());
+
+        return patient;
     }
 }
