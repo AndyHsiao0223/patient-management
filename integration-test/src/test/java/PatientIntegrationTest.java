201b77ec762283328c6fc53abb5ed4ed846ae2a8
AndyHsiao0223 <Andy90.2.23@gmail.com>
2026-05-01 13:19:09 +0800

    feat: Add integration tests for patient retrieval with valid token

--- integration-test/src/test/java/PatientIntegrationTest.java
+++ integration-test/src/test/java/PatientIntegrationTest.java
@@ -1,19 +1,18 @@
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
-public class AuthIntegrationTest  {
+public class PatientIntegrationTest {
     @BeforeAll
     static void setUp() {
         RestAssured.baseURI = "http://localhost:4004";
     }
 
     @Test
-    public void shouldReturnUnauthorizedWithValidToken() {
+    public void shouldReturnPatientsWithValidToken() {
         String loginPayload = """
                 {
                     "email": "testuser@example.com",
@@ -22,7 +21,7 @@ public class AuthIntegrationTest  {
             """;
 
             // Arrange
-        Response response = given()
+        String token = given()
             .contentType("application/json")
             .body(loginPayload)
             // Act
@@ -31,32 +30,17 @@ public class AuthIntegrationTest  {
             // Assert
             .then()
             .statusCode(200)
-            .body("token", notNullValue())
             // Extract response
             .extract()
-            .response();
+            .jsonPath()
+            .get("token");
 
-        System.out.println("Generated Token: " + response.jsonPath().getString("token"));
-    }
-
-    @Test
-    public void shouldReturnUOnInvalidLogin() {
-        String loginPayload = """
-                {
-                    "email": "invalidUser@example.com",
-                    "password": "wrongPassword"
-                }
-            """;
-
-        // Arrange
         given()
-            .contentType("application/json")
-            .body(loginPayload)
-            // Act
+            .header("Authorization", "Bearer " + token)
             .when()
-            .post("/api/auth/login")
-            // Assert
+            .get("/api/patients")
             .then()
-            .statusCode(401);
+            .statusCode(200)
+            .body("patients", notNullValue());
     }
 }
